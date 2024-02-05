fae8c174d8c1bb15bdd1660d046f58cf82a9aea4
gcthechamp <gopalsatyam35@gmail.com>
2024-02-06 00:30:01 +0530

    Clone Upgraded #2

--- script.js
+++ script.js
@@ -3,7 +3,32 @@ toggle.addEventListener("click",onClickToggle);
 
 const others = document.getElementById("others");
 
+
+const hamburger = document.getElementById("hamburger_icon");
+const hamburgerPopup = document.getElementById("hamburger-popup");
+hamburger.addEventListener("click", onHamClick);
+
 function onClickToggle()
 {
-    others.classList.toggle("display-block");
+    let style = window.getComputedStyle(others);
+    let display = style.getPropertyValue('display');
+    if(display == "none")
+    {
+     others.style.display = "block";
+     toggle.innerText = "-";
+    } 
+    else  
+    {
+     others.style.display = "none";
+     toggle.innerText = "+";
+    }
+}    
+
+
+
+
+
+function onHamClick()
+{
+  hamburgerPopup.classList.toggle("hide");
 }
